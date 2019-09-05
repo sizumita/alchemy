673da3b810e3ec20bf258095852acde6b15fa439
dongmaster <bin@tomoko.moe>
2019-09-06 00:08:54 +0200

    changed opusenc to ffmpeg

--- lib/Voice/controller.ex
+++ lib/Voice/controller.ex
@@ -227,9 +227,9 @@ defmodule Alchemy.Voice.Controller do
   defp opusenc(data, options) do
     opts = [in: data, out: :stream]
 
-    opusenc_command = ["opusenc", "--quiet", "-", "-"]
+    ffmpeg_command = ["-hide_banner", "-loglevel", "quiet", "-i","pipe:0" "-f", "data", "-map", "0:a", "-ar", "48k", "-ac", "2", "-acodec", "libopus", "-b:a", "128k", "pipe:1"]
 
-    %Proc{out: opusenc_out} = Porcelain.spawn("opusenc", opusenc_command, opts)
+    %Proc{out: opusenc_out} = Porcelain.spawn(Application.fetch_env!(:alchemy, :ffmpeg_path), ffmpeg_command, opts)
     opusenc_out
   end
 
